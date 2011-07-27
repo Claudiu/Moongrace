ed4ea6d8f6a57d41c7f268746383d13efba68869
Claudiu Tașcă <claudiu@rubynize.net>
2011-07-27 22:29:05 +0300

    Working

--- Application/Views/index.php
+++ Application/Views/index.php
@@ -2,12 +2,50 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
-	<title></title>
+	<title>Moongrace PHP5 Framework</title>
+	<style type="text/css">
+		body {
+			font-size: 15px;
+			font-family:Arial, Helvetica, sans-serif;
+			background-color: #eef8ff;
+			line-height: 1.5em;
+			color: #07242F
+		}
+		
+		h1 {
+			color: #e50404;
+			text-shadow:-1px 1px 1px #d50303;
+		}
+		
+		code {
+			display: block;
+			border: 1px dashed #167396 ;
+			color: #2d2d2d;
+			padding: 5px 10px;
+			margin-top: 10px;
+			font-family: Monospace;
+		}
+		
+		.wrap {
+			width: 960px;
+			margin: 70px auto;
+			padding: 20px;
+			background-color: #ddf1ff;
+			border: 5px solid #ccebff;
+			-webkit-border-radius: 5px;
+			-moz-border-radius: 5px;
+			border-radius: 5px;
+		}
+	</style>
 </head>
 <body>
 	<div class="wrap">
 		<h1>You have successfully installed Moongrace</h1>
-		<p></p>
+		<p>Thank you for choosing the Moongrace PHP Framework for your project. If you see this message, you are all set to go, so go out there and build your way to the moon.</p>
+		<p>
+			You can edit this page in here:
+			<code>Application/Views/index.php</code>
+		</p>
 	</div>
 </body>
 </html>
