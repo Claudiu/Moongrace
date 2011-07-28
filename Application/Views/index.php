7bc20dcac0c05f1109475f789d637185a8eab6b9
Claudiu Tașcă <claudiu@rubynize.net>
2011-07-28 15:57:33 +0300

    New default index typography style

--- Application/Views/index.php
+++ Application/Views/index.php
@@ -6,22 +6,25 @@
 	<style type="text/css">
 		body {
 			font-size: 15px;
-			font-family:Arial, Helvetica, sans-serif;
+			font-family:"MS Sans Serif", Geneva, sans-serif;
 			background-color: #eef8ff;
-			line-height: 1.5em;
-			color: #07242F
+			line-height: 0.9em;
+			color: #3b3b3b;
+			
 		}
 		
 		h1 {
-			color: #e50404;
-			text-shadow:-1px 1px 1px #d50303;
+			font-family: Georgia, serif;
+			font-size: 24px;
+			font-weight: normal;
+			font-style:italic;
+			color: #2d2d2d;
 		}
 		
 		code {
 			display: block;
-			border: 1px dashed #167396 ;
 			color: #2d2d2d;
-			padding: 5px 10px;
+			padding: 10px 0px;
 			margin-top: 10px;
 			font-family: Monospace;
 		}
@@ -30,8 +33,6 @@
 			width: 960px;
 			margin: 70px auto;
 			padding: 20px;
-			background-color: #ddf1ff;
-			border: 5px solid #ccebff;
 			-webkit-border-radius: 5px;
 			-moz-border-radius: 5px;
 			border-radius: 5px;
@@ -40,8 +41,8 @@
 </head>
 <body>
 	<div class="wrap">
-		<h1>You have successfully installed Moongrace</h1>
-		<p>Thank you for choosing the Moongrace PHP Framework for your project. If you see this message, you are all set to go, so go out there and build your way to the moon.</p>
+		<h1>You have successfully installed Moongrace.</h1>
+		<p>Thank you for choosing the Moongrace PHP Framework for your project.</p>
 		<p>
 			You can edit this page in here:
 			<code>Application/Views/index.php</code>
